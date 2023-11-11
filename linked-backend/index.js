de91e1dd3b6ae6df7104de503be6d45d6eae8513
compli <shirothegamer69@gmail.com>
2023-11-11 12:42:43 +0530

    removed cors

--- linked-backend/index.js
+++ linked-backend/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const Data = require("./models/data");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ exposedHeaders: "token" }));
 
 
 mongoose
