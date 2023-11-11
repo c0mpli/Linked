4ccc436ed18eaa38e712314f9210dbafdbd8a2bd
compli <shirothegamer69@gmail.com>
2023-11-11 12:41:00 +0530

    minor changes

--- linked-backend/index.js
+++ linked-backend/index.js
@@ -10,7 +10,7 @@ app.use(cors({ exposedHeaders: "token" }));
 
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to database!"))
   .catch((error) => console.log("Failed to connect to database!", error));
 
